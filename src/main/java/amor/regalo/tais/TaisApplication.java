43390dfb520039e9623d0b51f70406ccbf5f7b94
Tomas Medel <tfmedel@gmail.com>
2024-09-02 10:21:35 -0300

    fotos v1

---
+++ src/main/java/amor/regalo/tais/TaisApplication.java
@@ -0,0 +1,13 @@
+package amor.regalo.tais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amor/regalo/tais/TaisApplicationTests.java
@@ -0,0 +1,13 @@
+package amor.regalo.tais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
